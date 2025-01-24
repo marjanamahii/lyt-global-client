b34760f38bb6dcc13ae8d6a6378d3f85a444b788
Marjana Begum <Marjana@MacBook-Air.local>
2025-01-24 06:09:36 +0000

    dashboard added

--- src/routers/PrivateRoutes.jsx
+++ src/routers/PrivateRoutes.jsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { Navigate, useLocation } from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
 
-// const PrivateRoutes = ({ children, role }) => {
-//   const { user } = useSelector((state) => state.auth);
-//   const location = useLocation();
-//   if (!user) {
-//     alert("You must be logged in!");
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
+const PrivateRoutes = ({ children, role }) => {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+  if (!user) {
+    alert("You must be logged in!");
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-//   if (role && user.role !== role) {
-//     alert("You are not authorized to access this page!");
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
+  if (role && user.role !== role) {
+    alert("You are not authorized to access this page!");
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-//   return children;
-// };
+  return children;
+};
 
-// export default PrivateRoutes;
+export default PrivateRoutes;
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -10,6 +10,9 @@ import Register from "../components/Register/Register";
 import Services from "../pages/Services/Services";
 import Events from "../pages/Events/Events";
 import SearchBar from "../components/SearchBar/SearchBar";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import UserProfile from "../pages/Dashboard/User/UserProfile";
+import PrivateRoutes from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
@@ -32,17 +35,6 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login />,
       },
-      // { path: "/search", element: <Search /> },
-      // { path: "/shop", element: <ShopPage /> },
-      // { path: "/shop/:id", element: <SingleProduct /> },
-      // {
-      //   path: "/success",
-      //   element: <PaymentSuccess />,
-      // },
-      // {
-      //   path: "/orders/:orderId",
-      //   element: <OrderDetails />,
-      // },
     ],
   },
 
@@ -51,72 +43,72 @@ const router = createBrowserRouter([
   //   element: <Chatbot />,
   // },
   // Dashboard Starts
-  // {
-  //   path: "/dashboard",
-  //   element: (
-  //     <PrivateRoutes>
-  //       <Dashboard />
-  //     </PrivateRoutes>
-  //   ),
-  //   children: [
-  //     // user routes
-  //     { path: "", element: <UserDashMain /> },
-  //     { path: "orders", element: <UserOrders /> },
-  //     { path: "payments", element: <UserPayments /> },
-  //     { path: "profile", element: <UserProfile /> },
-  //     { path: "reviews", element: <UserReviews /> },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoutes>
+        <Dashboard />
+      </PrivateRoutes>
+    ),
+    children: [
+      // user routes
+      // { path: "", element: <UserDashMain /> },
+      // { path: "orders", element: <UserOrders /> },
+      // { path: "payments", element: <UserPayments /> },
+      { path: "profile", element: <UserProfile /> },
+      // { path: "reviews", element: <UserReviews /> },
 
-  //     // admin routes (only accessible by admin)
-  //     {
-  //       path: "admin",
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <AdminDashMain />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //     {
-  //       path: "add-product",
+      // admin routes (only accessible by admin)
+      // {
+      //   path: "admin",
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <AdminDashMain />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+      // {
+      //   path: "add-product",
 
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <AddProduct />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //     {
-  //       path: "manage-products",
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <ManageProduct />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //     {
-  //       path: "update-product/:id",
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <UpdateProduct />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //     {
-  //       path: "users",
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <ManageUser />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //     {
-  //       path: "manage-orders",
-  //       element: (
-  //         <PrivateRoutes role="admin">
-  //           <ManageOrders />
-  //         </PrivateRoutes>
-  //       ),
-  //     },
-  //   ],
-  // },
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <AddProduct />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+      // {
+      //   path: "manage-products",
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <ManageProduct />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+      // {
+      //   path: "update-product/:id",
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <UpdateProduct />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+      // {
+      //   path: "users",
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <ManageUser />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+      // {
+      //   path: "manage-orders",
+      //   element: (
+      //     <PrivateRoutes role="admin">
+      //       <ManageOrders />
+      //     </PrivateRoutes>
+      //   ),
+      // },
+    ],
+  },
 ]);
 export default router;
