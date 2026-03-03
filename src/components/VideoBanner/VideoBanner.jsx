f71460f4250a1a549096a68e0441350b7ac491cb
Marjana Begum <Marjana@MacBook-Air.local>
2026-03-03 23:37:49 +0000

    video banner updated

--- src/components/VideoBanner/VideoBanner.jsx
+++ src/components/VideoBanner/VideoBanner.jsx
@@ -1,58 +1,112 @@
+// /* eslint-disable no-unused-vars */
+// import React from "react";
+// import "./VideoBanner.css";
+// import vidBg2 from "../../assets/vid/bannervid.mp4";
+// import { ImFacebook } from "react-icons/im";
+// import { AiOutlineLinkedin } from "react-icons/ai";
+// import { AiFillYoutube } from "react-icons/ai";
+// // import { AiOutlineTwitter } from 'react-icons/ai';
+// import { RiTwitterXLine } from "react-icons/ri";
+// import { AiOutlineInstagram } from "react-icons/ai";
+// import { Button } from "react-bootstrap";
+// // import { Link, NavLink } from "react-router-dom";
+
+// const VideoBanner = () => {
+//   return (
+//     <div>
+//       <section className="showcase">
+//         <video src={vidBg2} muted loop autoPlay />
+//         <div className="text m-20">
+//           <h1>Start Your </h1>
+//           <h3>Path to Global Education</h3>
+//           <p>
+//             We take pride in our ability to help students achieve their academic
+//             goals and succeed in life.
+//           </p>
+
+//           {/* "ineed to add doamin here/register/"  */}
+//           <a href="/register" rel="noreferrer">
+//             <Button className="btn-apply-now">Contact Now</Button>
+//           </a>
+//         </div>
+//         <ul className="social">
+//           <li>
+//             <a
+//               href="https://www.facebook.com/profile.php?id=61562034145449"
+//               target="_blank"
+//               rel="noreferrer"
+//             >
+//               <ImFacebook style={{ fontSize: "46px", color: "#261919" }} />
+//             </a>
+//           </li>
+//           <li>
+//             <a
+//               href="https://www.linkedin.com/company/lyt-global/"
+//               target="_blank"
+//               rel="noreferrer"
+//             >
+//               <AiOutlineLinkedin
+//                 style={{ fontSize: "46px", color: "#261919" }}
+//               />
+//             </a>
+//           </li>
+//         </ul>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default VideoBanner;
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./VideoBanner.css";
 import vidBg2 from "../../assets/vid/bannervid.mp4";
 import { ImFacebook } from "react-icons/im";
 import { AiOutlineLinkedin } from "react-icons/ai";
-import { AiFillYoutube } from "react-icons/ai";
-// import { AiOutlineTwitter } from 'react-icons/ai';
-import { RiTwitterXLine } from "react-icons/ri";
-import { AiOutlineInstagram } from "react-icons/ai";
 import { Button } from "react-bootstrap";
-// import { Link, NavLink } from "react-router-dom";
 
 const VideoBanner = () => {
   return (
-    <div>
-      <section className="showcase">
-        <video src={vidBg2} muted loop autoPlay />
-        <div className="text m-20">
-          <h1>Start Your </h1>
-          <h3>Path to Global Education</h3>
-          <p>
-            We take pride in our ability to help students achieve their academic
-            goals and succeed in life.
-          </p>
+    <section className="video-banner">
+      <div className="video-overlay"></div>
+      <video src={vidBg2} muted loop autoPlay className="bg-video" />
 
-          {/* "ineed to add doamin here/register/"  */}
-          <a href="/register" rel="noreferrer">
-            <Button className="btn-apply-now">Contact Now</Button>
+      <div className="banner-content">
+        <h1 className="banner-title">
+          START YOUR <br />
+          <span className="highlight-text">PATH TO GLOBAL EDUCATION</span>
+        </h1>
+        <p className="banner-description text-white">
+          We take pride in our ability to help students achieve their academic
+          goals and succeed in life.
+        </p>
+
+        <a href="/register">
+          <Button className="btn-contact-now mt-4">Contact Now</Button>
+        </a>
+      </div>
+
+      <ul className="banner-social">
+        <li>
+          <a
+            href="https://www.facebook.com/profile.php?id=61562034145449"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ImFacebook />
           </a>
-        </div>
-        <ul className="social">
-          <li>
-            <a
-              href="https://www.facebook.com/profile.php?id=61562034145449"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <ImFacebook style={{ fontSize: "46px", color: "#261919" }} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/company/lyt-global/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiOutlineLinkedin
-                style={{ fontSize: "46px", color: "#261919" }}
-              />
-            </a>
-          </li>
-        </ul>
-      </section>
-    </div>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/company/lyt-global/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineLinkedin />
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 };
 
